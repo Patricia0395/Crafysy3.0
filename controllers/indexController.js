f98d1ff548ecf876c12b45826da6034e2318bb8b
EricM76 <53233372+EricM76@users.noreply.github.com>
2021-08-31 14:04:36 -0300

    agrega productos dinámicos en home

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+let  products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'))
+
 module.exports = {
     index : (req,res) => {
-        return res.render('index', { title: 'Craftsy 2.0' });
+        return res.render('index', { 
+            title: 'Craftsy 2.0',
+            products : JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'))
+        });
     },
     admin : (req,res) => {
         return res.render('admin')
