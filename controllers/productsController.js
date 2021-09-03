f8d1a7745d685c894cd51e17331f1c67befa95f2
EricM76 <53233372+EricM76@users.noreply.github.com>
2021-09-03 09:11:48 -0300

    agrega rutas parametrizadas

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,8 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+let  products = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','products.json'),'utf-8'))
+
+
 module.exports = {
     add : (req,res) => {
         return res.render('productAdd')
     },
     detail : (req,res) => {
-        return res.render('productDetail')
+    
+        return res.render('productDetail',{
+            product : products.find(product => product.id === +req.params.id)
+        })
     }
 }
