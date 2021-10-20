f55108c6b1f1652cb7aaf514fe5e25dc3c2d4bb4
Patricia Pacheco <patricia.soledad0395@gmail.com>
2021-10-20 15:54:09 -0300

    migraciones creadas

---
+++ src/database/migrations/20211020185034-create-product-feature.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('product_features', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productId: {
+        type: Sequelize.INTEGER
+      },
+      featureId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('product_features');
+  }
+};
